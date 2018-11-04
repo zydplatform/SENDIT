ceea819ecd49eda769059cc347e347d810369872
zydplatform <kigandaivan32@gmail.com>
2018-11-04 22:31:45 +0300

    ft-ui-features-modified-[Finishes#161705142]

---
+++ UI/static/main.js
@@ -0,0 +1,7 @@
+var login = document.getElementById("login");
+var signup = document.getElementById("signup");
+var name = document.getElementById("name");
+var email = document.getElementById("email");
+var contact = document.getElementById("contact");
+var location = document.getElementById("location");
+var password = document.getElementById("password");
