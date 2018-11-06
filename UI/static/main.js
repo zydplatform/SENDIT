885d37bd1e010f41fd16ec2e2399cb1927e5d55e
zydplatform <kigandaivan32@gmail.com>
2018-11-06 18:36:31 +0300

    ft-more-changes-in-html-files

--- UI/static/main.js
+++ UI/static/main.js
@@ -1,7 +1,37 @@
-var login = document.getElementById("login");
-var signup = document.getElementById("signup");
-var name = document.getElementById("name");
-var email = document.getElementById("email");
-var contact = document.getElementById("contact");
-var location = document.getElementById("location");
-var password = document.getElementById("password");
+// var login = document.getElementById("login");
+// var signup = document.getElementById("signup");
+// var name = document.getElementById("name");
+// var email = document.getElementById("email");
+// var contact = document.getElementById("contact");
+// var location = document.getElementById("location");
+// var password = document.getElementById("password");
+// login.addEventListener('submit', function(e){
+//     e.preventDefault();
+
+//     if (e.target.email.value == 'admin' && e.target.password.value == 'admin'){
+//         e.target.email.value = '';
+//         e.target.password.value = '';
+//         window.location.href = 'admin.html'
+//     } else if (e.target.email.value == 'client' && e.target.password.value == 'client'){
+//         e.target.email.value = '';
+//         e.target.email.value = '';
+//         window.location.href = 'user.html';
+//     }
+// });
+//     // if (e.target.username.value == '' || e.target.password.value == ''){
+//     //     wrongUsername.style.display = 'block';
+//     //     wrongUsername.innerText = 'Provide Username and Password';
+//     // } else if (e.target.username.value != 'admin' || e.target.password.value != 'admin'){
+//     //     wrongUsername.style.display = 'block';
+//     //     wrongUsername.innerText = 'wrong username or password';
+//     // }
+
+//     // if (e.target.username.value == '' || e.target.password.value == ''){
+//     //     wrongPassword.style.display = 'block';
+//     //     wrongPassword.innerText = 'Provide Username and Password';
+//     // } else if (e.target.username.value != 'attendant' || e.target.password.value != 'attendant'){
+//     //     wrongPassword.style.display = 'block';
+//     //     wrongPassword.innerText = 'wrong username or password';
+//     // }
+
+   
